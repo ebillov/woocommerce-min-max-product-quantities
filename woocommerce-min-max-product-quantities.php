5baf37bc4e781be4d87493575e9be5a8386f3cfb
ebillov <ebillov@gmail.com>
2020-04-03 10:11:36 +0800

    Updated license text.

--- woocommerce-min-max-product-quantities.php
+++ woocommerce-min-max-product-quantities.php
@@ -8,18 +8,18 @@ Author:      Virson Ebillo
 Author URI:  https://virson.wordpress.com/
 License:     GNUv3
  
-WooCommerce Tabulated Bulk Shop is free software: you can redistribute it and/or modify
+WooCommerce Min & Max Product Quantities is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 any later version.
  
-WooCommerce Tabulated Bulk Shop is distributed in the hope that it will be useful,
+WooCommerce Min & Max Product Quantities is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with WooCommerce Tabulated Bulk Shop. If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+along with WooCommerce Min & Max Product Quantities. If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
 */
 
 //Exit if accessed directly.
